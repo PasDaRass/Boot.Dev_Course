4209a63d63a241794eff3a7e7b5472ff167d29d5
Pascal R <48902056+PasDaRass@users.noreply.github.com>
2025-02-27 03:16:55 +0000

    Create lesson02.py

---
+++ 01_python/chapter2/lesson02.py
@@ -0,0 +1,19 @@
+player_health = 1000
+
+# reduce by 100 here
+player_health = player_health - 100
+''' OR player_health = 900 '''
+print(player_health)
+
+# and here
+player_health -= 100
+''' OR player_health = 800 '''
+print(player_health)
+
+# and here
+player_health = player_health - 100
+print(player_health)
+
+# and here
+player_health = player_health - 100
+print(player_health)
