404e77e58901f2cfd4be6f4645ee08b2e8594ebb
Pascal R <48902056+PasDaRass@users.noreply.github.com>
2025-03-04 04:01:09 +0000

    Create lesson10.py

---
+++ 01_python/chapter3/lesson10.py
@@ -0,0 +1,6 @@
+"""
+Quiz
+
+Q: Which is best practice to make sure you don't define functions out of order?
+A: Create an entrypoint function (usually called main) and call it at the end of the file
+"""
