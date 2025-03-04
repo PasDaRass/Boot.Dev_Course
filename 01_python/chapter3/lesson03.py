ae840b6c3c81589293fffc13d9fbdb5a9fe4a04c
Pascal R <48902056+PasDaRass@users.noreply.github.com>
2025-03-04 01:35:11 +0000

    Update lesson03.py

--- 01_python/chapter3/lesson03.py
+++ 01_python/chapter3/lesson03.py
@@ -1,6 +1,6 @@
 """
 QUIZ
 
-Q: Functions - What happens when the code is executed
-A: The function is called
+Q: Functions - What happens when the code is executed?
+A: A value is returned from the function
 """
