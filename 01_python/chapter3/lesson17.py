3b4465af4a980845fbccd610ab3cc2319c932208
Pascal R <48902056+PasDaRass@users.noreply.github.com>
2025-03-04 04:45:11 +0000

    Create lesson17.py

---
+++ 01_python/chapter3/lesson17.py
@@ -0,0 +1,8 @@
+"""
+Quiz
+
+Q: What's the difference between printing a value and returning it?
+A:
+Print: Shows a value to the console
+Return: Makes a value available to the caller of the function
+"""
