27c00fc8e4e6c553be51deb26dac9db8fc5c2bac
Pascal R <48902056+PasDaRass@users.noreply.github.com>
2025-03-04 01:45:35 +0000

    Create lesson04.py

---
+++ 01_python/chapter3/lesson04.py
@@ -0,0 +1,16 @@
+"""
+Quiz
+
+def area_of_circle(r):
+    pi = 3.14
+    result = pi * r * r
+    return result
+
+radius = 5
+area = area_of_circle(radius)
+print(area)
+# 78.5
+
+Q: Why is the variable called 'radius' outside the function, but 'r' inside the function?
+A: Because only the value of the variable is passed to the function. It is then assigned a new variable called 'r'
+"""
