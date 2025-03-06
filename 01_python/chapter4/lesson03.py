48c96845816438ecbadd31167d25b7f0ca80c575
Pascal R <48902056+PasDaRass@users.noreply.github.com>
2025-03-06 01:02:36 +0000

    Create lesson03.py

---
+++ 01_python/chapter4/lesson03.py
@@ -0,0 +1,13 @@
+"""
+QUIZ
+
+pi = 3.14
+
+def get_area_of_circle(radius):
+    area = pi * radius * radius
+    return area
+===========
+
+Q: Does the get_area_of_circle function have access to the pi variable?
+A: 
+"""
