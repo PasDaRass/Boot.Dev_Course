b998e5dbf6068155aabb60ae9dca42bf9aaf67ed
Pascal R <48902056+PasDaRass@users.noreply.github.com>
2025-03-07 01:23:40 +0000

    Create lesson04.py

---
+++ 01_python/chapter4/lesson04.py
@@ -0,0 +1,12 @@
+"""
+Quiz
+
+pi = 3.14
+
+def get_area_of_circle(radius):
+    area = pi * radius * radius
+    return area
+
+Q: Can the area variable inside the get_area_of_circle function be accessed outside of the function?
+A: No, variables defined inside a function are not accessible outside of it
+"""
