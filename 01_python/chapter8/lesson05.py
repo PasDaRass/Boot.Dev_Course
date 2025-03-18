766bf926952327dc548a8096daac2873f4aab12e
Pascal R <48902056+PasDaRass@users.noreply.github.com>
2025-03-18 03:42:05 +0000

    Create lesson05.py

---
+++ 01_python/chapter8/lesson05.py
@@ -0,0 +1,6 @@
+"""
+QUIZ
+
+Q: Why does Python loop through the range function without iterating through the final number?
+A: Because print(i) only happens when (i) is less than YET NOT equal to the final number
+"""
