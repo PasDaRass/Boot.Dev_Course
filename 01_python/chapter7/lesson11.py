592575c20f67d33a3ce4385a4eaaee11f0d10eae
Pascal R <48902056+PasDaRass@users.noreply.github.com>
2025-03-18 02:01:18 +0000

    Create lesson11.py

---
+++ 01_python/chapter7/lesson11.py
@@ -0,0 +1,5 @@
+def should_serve_customer(customer_age, on_break, time):
+    if((customer_age >= 21) and not (on_break) and (5 <= time <= 10)):
+        return True
+    else:
+        return False
