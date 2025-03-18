86763ea2fd0ab6ce5b38e9485ce33231625f5100
Pascal R <48902056+PasDaRass@users.noreply.github.com>
2025-03-18 01:50:06 +0000

    Create lesson10.py

---
+++ 01_python/chapter7/lesson10.py
@@ -0,0 +1,12 @@
+"""
+QUIZ
+
+is_tall = True
+is_fat = True
+is_skinny = False
+is_short = False
+result = (is_tall and is_fat) or (is_skinny and is_short)
+
+Q: What is the value of /result/?
+A: True
+"""
