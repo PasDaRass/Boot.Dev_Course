e961d129160ea31d60e863979af5a6866dd882e3
Pascal R <48902056+PasDaRass@users.noreply.github.com>
2025-03-21 02:04:43 +0000

    Create lesson10.py

---
+++ 01_python/chapter9/lesson10.py
@@ -0,0 +1,18 @@
+def get_item_counts(items):
+    potion_count = 0
+    bread_count = 0
+    shortsword_count = 0
+
+    # don't touch above this line
+
+    for i in items:
+        if i == "Potion":
+            potion_count += 1
+        elif i == "Bread":
+            bread_count += 1
+        elif i == "Shortsword":
+            shortsword_count += 1
+
+    # don't touch below this line
+
+    return potion_count, bread_count, shortsword_count
