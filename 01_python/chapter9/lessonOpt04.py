8b89435d73037cb658781b73e8c26769df262f89
Pascal R <48902056+PasDaRass@users.noreply.github.com>
2025-03-23 16:58:10 +0000

    Update lessonOpt04.py

--- 01_python/chapter9/lessonOpt04.py
+++ 01_python/chapter9/lessonOpt04.py
@@ -2,10 +2,11 @@ def validate_path(expected_path, character_path):
     character_name = character_path[0]
     waypoints = 0
     index = 0
-    del character_path[0]
+    # del character_path[0]
     
     for i in expected_path:
-        for x in character_path:
+        for x in character_path[1:]:
+        # for x in character_path:
             if i == x:
                 waypoints += 1
 
