685f3ab9942e5ecd23f7075b1b4987725e7d2b24
Pascal R <48902056+PasDaRass@users.noreply.github.com>
2025-03-27 00:28:03 +0000

    Update lesson09.py

--- 01_python/chapter12/lesson09.py
+++ 01_python/chapter12/lesson09.py
@@ -1,6 +1,13 @@
 """
 QUIZ
 
+try:
+    raise Exception("zero division")
+except ZeroDivisionError as e:
+    print("zero")
+except Exception as e:
+    print("other")
+
 Q: In the code sample, what will happen?
 A: The program will crash with an uncaught exception
 """
