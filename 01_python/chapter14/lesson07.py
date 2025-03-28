c14c57ad776f582e7add6b7ed37e6a9ce7209a64
Pascal R <48902056+PasDaRass@users.noreply.github.com>
2025-03-28 03:19:50 +0000

    Create lesson07.py

---
+++ 01_python/chapter14/lesson07.py
@@ -0,0 +1,12 @@
+"""
+QUIZ
+
+Python 2 example
+print "hello world"
+Python 3 example
+print("hello world")
+
+print "python 2 really needs to die already"
+--> print("python 2 really needs to die already")
+
+"""
