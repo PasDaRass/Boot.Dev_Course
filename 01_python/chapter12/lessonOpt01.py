a680bd2fd368538134669a7bbc7a76f7182157ad
Pascal R <48902056+PasDaRass@users.noreply.github.com>
2025-03-28 01:59:11 +0000

    Update lessonOpt01.py

--- 01_python/chapter12/lessonOpt01.py
+++ 01_python/chapter12/lessonOpt01.py
@@ -1,1 +1,5 @@
-
+def purchase_item(price, gold_available):
+        if price <= gold_available:
+            return gold_available - price
+        else:
+            raise Exception("not enough gold")
