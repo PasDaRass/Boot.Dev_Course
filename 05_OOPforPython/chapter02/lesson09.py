484a541416cfaf38e473510015254b05f935655a
Pascal R <48902056+PasDaRass@users.noreply.github.com>
2025-05-03 00:04:08 +0100

    Create lesson09.py

---
+++ 05_OOPforPython/chapter02/lesson09.py
@@ -0,0 +1,18 @@
+"""
+QUIZ
+
+Q: Which of the following is true?
+
+1 - An object is an instance of a class
+
+
+2 - A method is an instance of a function
+
+
+3 - A function is an instance of a method
+
+
+4 - A class is an instance of an object
+
+A: 1 
+"""
