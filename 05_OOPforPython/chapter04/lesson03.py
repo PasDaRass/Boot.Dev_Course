e83a03081f8d273106da6ba186d5aad3b3e47bb6
Pascal R <48902056+PasDaRass@users.noreply.github.com>
2025-05-05 23:12:56 +0100

    Create lesson03.py

---
+++ 05_OOPforPython/chapter04/lesson03.py
@@ -0,0 +1,6 @@
+"""
+QUIZ
+
+Q: Abstraction emphasizes ____ and encapsulation emphasizes ____
+A: Exposing essential features whilst hiding complexity, bundling data with methods and restricting access to inner workings
+"""
