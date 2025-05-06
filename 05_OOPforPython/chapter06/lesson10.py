ba16c641c19c3f7b51d9bc98d0f8cfe937dc772a
Pascal R <48902056+PasDaRass@users.noreply.github.com>
2025-05-06 18:09:59 +0100

    Create lesson10.py

---
+++ 05_OOPforPython/chapter06/lesson10.py
@@ -0,0 +1,23 @@
+"""
+QUIZ
+
+
+Operation	Operator	Method
+Addition	+	__add__
+Subtraction	-	__sub__
+Multiplication	*	__mul__
+Power	**	__pow__
+Division	/	__truediv__
+Floor Division	//	__floordiv__
+Remainder (modulo)	%	__mod__
+Bitwise Left Shift	<<	__lshift__
+Bitwise Right Shift	>>	__rshift__
+Bitwise AND	&	__and__
+Bitwise OR	|	__or__
+Bitwise XOR	^	__xor__
+Bitwise NOT	~	__invert__
+
+
+Q: Which is the inverse of __mul__?
+A: __truediv__
+"""
